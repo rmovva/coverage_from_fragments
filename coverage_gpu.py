a11e5bf855dedeec1b829a388ac441c39d2a3510
Raj Movva <rmovva@nvidia.com>
2020-08-13 17:21:39 -0700

    Notebooks use latest coverage_gpu, AtacWorks section is streamlined

--- coverage_gpu.py
+++ coverage_gpu.py
@@ -1,24 +1,37 @@
 # %%
 from subprocess import Popen, PIPE
+
+import os
 import cudf
-import pandas as pd
-import numpy as np
-from numba import cuda
 import time
+import tabix
+import numpy as np
+import pandas as pd
+from numba import cuda
 
-FRAGMENTS_FILE = './coverage_from_fragments/example.bed'
-GROUPING_FILE = './coverage_from_fragments/grouping_table.txt'
+import gzip
 
-COLUMN_NAMES = ['chrom', 'start', 'end', 'cell']
+from collections.abc import Sequence
 
 
-def query_fragments(df, chrom_map, chrom, start, end):
+def query_fragments(fragment_file, chrom, start, end):
+    tb = tabix.open(fragment_file)
+    results = tb.querys("%s:%d-%d" % (chrom, start, end))
+    records = []
+    for record in results:
+        records.append(record)
+    return records
+
+
+def tabix_query(filename, chrom, start, end):
     """Call tabix and generate an array of strings for each line it returns."""
-
-    chrHash = chrom_map[chrom]
-    fdf = df.query('chrom_hashed == @chrHash and start >= ' +
-                   str(start) + ' and end <= ' + str(end))
-    return fdf
+    query = '{}:{}-{}'.format(chrom, start, end)
+    process = Popen(['tabix', '-f', filename, query], stdout=PIPE)
+    records = []
+    for line in process.stdout:
+        record = line.decode('utf-8').strip().split('\t')
+        records.append(record)
+    return records
 
 
 def get_aggregate_score(cells_in_cluster, sum_inversed_reads, normalized_total):
@@ -26,11 +39,6 @@ def get_aggregate_score(cells_in_cluster, sum_inversed_reads, normalized_total):
     # also normalized by total number of cells in its cluster
     for i in range(cuda.threadIdx.x, len(cells_in_cluster), cuda.blockDim.x):
         normalized_total[i] = sum_inversed_reads[i] * (1.0 / cells_in_cluster[i])
-    
-    # values = group['value']
-    # total_cell_reads = group['total_reads']
-    # cells_in_cluster = group['cells_in_cluster']
-    # return np.sum(1 / total_cell_reads) * (1.0 / cells_in_cluster[0])
 
 
 def inverse(total_reads, inversed_reads):
@@ -52,50 +60,79 @@ def expand_interval(start, end, index, end_index,
             interval_index[j] = index[i]
 
 
-def get_coverages(df, chrom_map, chrom, start, end,
-                  group_file, resolution=1):
+def create_or_read_grouping_file(fragment_file, cluster_file, group_file):
+
+    if os.path.exists(group_file):
+        gdf = cudf.read_csv(group_file)
+    else:
+        fdf = cudf.read_csv(
+            gzip.open(fragment_file), sep='\t', 
+            names=['chrom', 'start', 'end', 'cell', 'duplicate'])
+
+        cell_cnt_series = fdf.groupby(['cell'])['cell'].count()
+        cell_gdf = cudf.DataFrame(
+            {'cell':cell_cnt_series.index, 
+            'total_reads':cell_cnt_series.values})
+
+        cdf = cudf.read_csv(cluster_file)
+        cdf.rename({'Barcode': 'cell', 'Cluster': 'group'}, inplace=True)
+
+        gdf = cell_gdf.merge(cdf, on=['cell'])
+        gdf.to_csv(group_file, index=False)
+    return gdf
+
+
+def read_data(chrom, start, end, fragment_file, cluster_file, group_file):
     t0 = time.time()
-    reads = query_fragments(df, chrom_map, chrom, start, end)
-
+    #Create a DF from the output of tablix query
+    reads = cudf.DataFrame(
+        data=tabix_query(fragment_file, chrom, start, end),
+        columns=['chrom', 'start', 'end', 'cell', 'duplicate'])
+    reads['row_num'] = reads.index
     print("Tabix query took %.2fs" % (time.time() - t0))
-    grouping = cudf.read_csv(group_file, sep='\t')
-    # print(grouping.head())
 
-    t_start = time.time()
+    t0 = time.time()
+    # Read grouping file
+    grouping = create_or_read_grouping_file(fragment_file, cluster_file, group_file)
+    print("Reading group file took %.2fs" % (time.time() - t0))
+
+    t0 = time.time()
+    reads = reads[reads['cell'].isin(grouping['cell'].tolist())]
+    reads = reads.astype({"start": np.int32, "end": np.int32})
+    # print(reads.shape)
+    print("Filtering reads took and type conversion %.2fs" % (time.time() - t0))
+
+    return reads, grouping
+
+
+def get_coverages(chrom, start, end,
+                  fragment_file, cluster_file, group_file, resolution=1):
+    reads, grouping = read_data(
+        chrom, start, end, fragment_file, cluster_file, group_file)
 
     # Add column with count of cells in cluster
     t0 = time.time()
-    grp_cnt = grouping.groupby(['group'], as_index=False).count()
-    # grp_cnt has counts of cell and total_reads. 
-    # Drop Total_reads and rename 'cell' label to something relavent(cells_in_cluster)
     print('Computing cell count in group/cluster...')
-    grp_cnt.drop(['total_reads'], inplace=True)
-    grp_cnt.rename({'cell': 'cells_in_cluster'}, inplace=True)
+    grp_cnt_series = grouping.groupby(['group'], as_index=False)['group'].count()
+    grp_cnt = cudf.DataFrame({
+        'group': grp_cnt_series.index, 
+        'cells_in_cluster': grp_cnt_series.values})
 
+    # grp_cnt has counts of cell and total_reads.
+    # Drop Total_reads and rename 'cell' label to something relavent(cells_in_cluster)
     grouping = grouping.merge(grp_cnt, on=['group'])
-    grouping.fillna({'cells_in_cluster': 0}, inplace=True)
-
-    print("groupBy cluster took %.2fs" % (time.time() - t0))
-    
-    # Filter df to only include reads for cells in groups file
-    t0 = time.time()
-    reads = reads[reads['cell'].isin(grouping['cell'])]
-    print("filtering reads took %.2fs" % (time.time() - t0))
+    del grp_cnt
+    print("GroupBy cluster took %.2fs" % (time.time() - t0))
 
     # Add total reads per cell as a column for each read in reads df
     t0 = time.time()
-
     reads = reads.merge(grouping, on=['cell'])
+    del grouping
     # print(reads.head())
-
-    print("merging groups with reads took %.2fs" % (time.time() - t0))
-
-    # Assign each read a value of 1 (placeholder in case we would like to weight reads later)
-    reads['value'] = 1
+    print("Merging groups with reads took %.2fs" % (time.time() - t0))
 
     # Expand reads to preferred resolution
     t0 = time.time()
-
     reads['diff'] = reads['end'] - reads['start']
     sumdf = reads['diff'].cumsum()
 
@@ -119,71 +156,56 @@ def get_coverages(df, chrom_map, chrom, start, end,
     )
 
     # Drop duplicate columns
-    reads.drop(['start', 'end'], inplace=True)
+    reads.drop(['start', 'end', 'diff'], inplace=True)
     intervals = intervals.merge(reads, on='row_num')
     intervals.drop(['row_num'], inplace=True)
-    # print(intervals.head())
+    del reads
 
+    # Create an column to store inverse of total_reads.
     intervals = intervals.apply_rows(inverse, 
         incols=['total_reads'], 
         outcols={'inversed_reads': np.float}, kwargs={})
+    intervals.drop(['total_reads'], inplace=True)
+    
     # print(intervals.head())
-
     print("expanding reads to bp resolution took %.2fs" % (time.time() - t0))
 
-    # Combine reads at shared positions for a given cluster
     t0 = time.time()
 
-    intervals_repl = intervals.copy()
-    intervals_repl.drop(
-        ['chrom_hashed', 'total_reads', 'cells_in_cluster', 'value', 'diff'],
-        inplace=True)
-    reads_sum_group = intervals_repl.groupby(
+    # Sum inversed_reads by grouping 'chrom', 'start', 'end', 'group'
+    # TODO: Is there a way to sum() just one column and merge it back to the df?
+    # print(intervals.head())
+    reads_sum_group = intervals.groupby(
         ['chrom', 'start', 'end', 'group'], as_index=False).sum()
     reads_sum_group.rename({'inversed_reads': 'sum_inversed_reads'}, inplace=True)
-
+    reads_sum_group.drop(['cells_in_cluster'], inplace=True)
+    
     intervals = intervals.merge(reads_sum_group, on=['chrom', 'start', 'end', 'group'])
-    # print(intervals)
-
-    grouped = intervals.groupby(
-        ['chrom', 'start', 'end', 'group'], as_index=False)
-
+    grouped = intervals.groupby(['chrom', 'start', 'end', 'group'], as_index=False)
+    
     intervals = grouped.apply_grouped(
         get_aggregate_score,
         incols=['cells_in_cluster', 'sum_inversed_reads'],
         outcols={'normalized_total': np.float})
-    # print(intervals.head())
 
-    intervals.reset_index(inplace=True)
+    intervals.drop(
+        ['cell', 'cells_in_cluster', 'inversed_reads', 'sum_inversed_reads'], 
+        inplace=True)
+
     print("getting scores per bp took %.2fs" % (time.time() - t0))
-    print("Total processing took %.2fs" % (time.time() - t_start))
-
     return intervals
-
-
-def initialize(fragments_file):
-
-    df = cudf.read_csv(fragments_file, sep='\t',
-                       header=None, names=COLUMN_NAMES)
-
-    df['chrom_hashed'] = df.hash_columns(['chrom'])
-    df['row_num'] = df.index  # Used to relate bed info to interval info
-
-    chrom_map = {}
-    for name, group in df.groupby(['chrom', 'chrom_hashed']):
-        chrom_map[name[0]] = name[1]
-
-    return df, chrom_map
 
 
 if __name__ == "__main__":
     t0 = time.time()
-    df, chrom_map = initialize(FRAGMENTS_FILE)
-    print("Initialization took %.2fs" % (time.time() - t0))
 
-    intervals = get_coverages(df, chrom_map,
-                              'chr11', 118202327, 118226182,
-                              GROUPING_FILE)
-    # print(intervals.head())
+    FRAGMENTS_FILE = './coverage/data/500//example.bed.gz'
+    GROUPING_FILE = './coverage_from_fragments/grouping_table.txt'
+
+    COLUMN_NAMES = ['chrom', 'start', 'end', 'cell']
+
+    intervals = get_coverages('chr11', 118202327, 118226182,
+                              FRAGMENTS_FILE, GROUPING_FILE)
+    print("Coverage took %.2fs" % (time.time() - t0))
 
 # %%
